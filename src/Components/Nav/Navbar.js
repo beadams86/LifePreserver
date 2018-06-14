e85986e64ed49dd39a27f81b164d51cbb184f9d7
beadams86 <bennett.adams@jspargo.com>
2018-06-14 09:44:55 -0400

    Initial commit with react app and scss

---
+++ src/Components/Nav/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <ul>
+            <li><a href="./">Home</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
